e2706a3463368a13f4f2219578fb7cec03d93765
Dan Wells <danvwells@gmail.com>
2017-11-18 16:23:07 -0700

    adding modal to open on select change, causing bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { MymodalComponent } from './mymodal/mymodal.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  entities = [{
+    id: 1,
+    name: "Something"
+  }, {
+    id: 2,
+    name: "Another thing"
+  }];
+  selectedId = 2;
+  
+  constructor(
+    private modalService: NgbModal
+  ) {}
+
+  onIdChange(entityId: string) {
+    let mymodal = this.modalService.open(MymodalComponent, {
+      keyboard: false,
+      backdrop: 'static'
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { MymodalComponent } from './mymodal/mymodal.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MymodalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    FormsModule
   ],
   providers: [],
+  entryComponents: [
+    MymodalComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
